cc670ab1b3d0c0407bb1a5488ad21b0c265eef8b
Kayo Almeida <kayo.almeida.dev@gmail.com>
2022-05-24 19:59:30 -0300

    fix permission script

--- lib/scripts/fix-permissions.sh
+++ lib/scripts/fix-permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for d in /var/www/*/ ; do
-  chown www-data:master -R /var/www/$d
-  find /var/www/$d -type d -print0 | sudo xargs -0 chmod 0775
-  find /var/www/$d -type f -print0 | sudo xargs -0 chmod 0664  
+  chown www-data:master -R $d
+  find $d -type d -print0 | sudo xargs -0 chmod 0775
+  find $d -type f -print0 | sudo xargs -0 chmod 0664  
 done
